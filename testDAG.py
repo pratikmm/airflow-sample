d7b8d0afbb64324f2df746f5160f9d9511ca1b39
mungp009 <Pratik.M.Mungekar.-ND@disney.com>
2021-01-13 19:06:56 +0530

    removed dummyop

--- testDAG.py
+++ testDAG.py
@@ -1,40 +1,28 @@
+import airflow
 from airflow import DAG
-from datetime import datetime, timedelta
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
-from airflow.operators.dummy_operator import DummyOperator
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime.utcnow(),
-    'email': ['airflow@example.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5)
+    'start_date': airflow.utils.dates.days_ago(0),
 }
 
-dag = DAG(
-    'testDAG', default_args=default_args, schedule_interval=timedelta(minutes=10))
+dag = DAG('sampleDAG', default_args=default_args, schedule_interval=None)
 
 
-start = DummyOperator(task_id='start', dag=dag)
-
-passing = KubernetesPodOperator(namespace='default',
-                          image="python:3.6",
-                          cmds=["python","-c"],
-                          arguments=["print('hello world')"],
-                          labels={"foo": "bar"},
-                          name="passing-test",
-                          task_id="passing-task",
-                          get_logs=True,
-                          dag=dag
-                          )
-
-
-end = DummyOperator(task_id='end', dag=dag)
+DriftTask = KubernetesPodOperator(
+    namespace='default',
+    image="python",
+    cmds=["python", "-c"],
+    arguments=["print('This code is running in a Kubernetes Pod')"],
+    labels={},
+    name="sampleDAG",
+    task_id="sampleDAG",
+    get_logs=True,
+    dag=dag,
+    log_events_on_failure=True,
+    is_delete_operator_pod=True)
 
 
-passing.set_upstream(start)
-passing.set_downstream(end)
-
+DriftTask
